b9ca94d2c23b748324333961c215f6f925d9a4dc
HorvathDavid <horvathdavid@Zsolt-MacBook-Air.local>
2014-09-20 10:57:26 +0200

    sd

--- player.js
+++ player.js
@@ -6,6 +6,14 @@ module.exports = {
   bet_request: function(game_state) {
   	console.log(game_state);
   	console.log(game_state.players[game_state.in_action].hole_cards);
+  	console.log(game_state.community_cards);
+
+  	var mycard = game_state.players[game_state.in_action].hole_cards;
+
+    var allcards = mycard.concat(game_state.community_cards);
+
+  	console.log(allcards);
+
 	var bet = game_state.players[game_state.in_action].bet || 0;
 
     return (game_state.current_buy_in - bet);
