0cfc8c6c51e39d1f6401a64376a879f6aa1c1758
HorvathDavid <horvathdavid@Zsolt-MacBook-Air.local>
2014-09-20 10:33:54 +0200

    sd

--- player.js
+++ player.js
@@ -5,7 +5,7 @@ module.exports = {
 
   bet_request: function(game_state) {
   	console.log(game_state.players);
-    return (game_state.current_buy_in - game_state.players[game_state.in_action].bet);
+    return (game_state.current_buy_in - game_state.players.Pogacsa.bet);
   },
 
   showdown: function(game_state) {
