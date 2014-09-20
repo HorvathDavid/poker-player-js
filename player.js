4602d9479f9bea2bf08ad971f3b41ede1bbca0ca
HorvathDavid <horvathdavid@Zsolt-MacBook-Air.local>
2014-09-20 10:27:44 +0200

    sd

--- player.js
+++ player.js
@@ -4,8 +4,8 @@ module.exports = {
   VERSION: "Default JavaScript folding player",
 
   bet_request: function(game_state) {
-  	
-    return game_state.current_buy_in;
+
+    return (game_state.current_buy_in-players.in_action.bet);
   },
 
   showdown: function(game_state) {
