67d97a241f89fc2ac5c4865977fc88f1fd5b90fa
HorvathDavid <horvathdavid@Zsolt-MacBook-Air.local>
2014-09-20 10:43:29 +0200

    sd:wq

--- player.js
+++ player.js
@@ -1,12 +1,12 @@
 
 module.exports = {
 
-  VERSION: "Default JavaScript folding player",
+  VERSION: "Pogacsa",
 
   bet_request: function(game_state) {
   	console.log(game_state);
-
-var bet = game_state.players[game_state.in_action].bet || 0;
+  	console.log(game_state.players[game_state.in_action].hole_cards);
+	var bet = game_state.players[game_state.in_action].bet || 0;
 
     return (game_state.current_buy_in - bet);
   },
